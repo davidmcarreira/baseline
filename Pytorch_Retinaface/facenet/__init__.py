622300a7f25f77ace94d68168897aa5b00a29447
david <davidmcarreira@protonmail.com>
2023-06-18 18:15:24 +0100

    second commit

---
+++ Pytorch_Retinaface/facenet/__init__.py
@@ -0,0 +1,9 @@
+from .models.inception_resnet_v1 import InceptionResnetV1
+from .models.utils import training
+
+import warnings
+warnings.filterwarnings(
+    action="ignore", 
+    message="This overload of nonzero is deprecated:\n\tnonzero()", 
+    category=UserWarning
+)
